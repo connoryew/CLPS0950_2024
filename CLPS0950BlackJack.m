a7dad30554471273edaac20eb5c6d061147aa0bf
Connor Yew <134974816+connoryew@users.noreply.github.com>
2024-03-06 10:28:50 -0500

    CLPS0950 BlackJack MATLAB File
    Created the template document for our BlackJack Group Project #1. Will fill in with an overview of the rules + elements of basic strategy

---
+++ CLPS0950BlackJack.m
@@ -0,0 +1,10 @@
+% CLPS0950 Group Project: How to Play Safe Blackjack (Basic Strategy Sim.)
+% Group Members: Nicole Chen, Jilienne Widener, Connor Yew
+
+% Rules of BlackJack
+% 1. 
+% 2. 
+% 3. 
+
+% Summary of Basic Strategy
+
