1cdfc578e2880544a82fc69ada24960140e9f0c2
Connor Yew <134974816+connoryew@users.noreply.github.com>
2024-03-06 13:26:45 -0500

    Update CLPS0950BlackJack.m
    Updated with more rules.

--- CLPS0950BlackJack.m
+++ CLPS0950BlackJack.m
@@ -2,9 +2,18 @@
 % Group Members: Nicole Chen, Jilienne Widener, Connor Yew
 
 % Rules of BlackJack
-% 1. 
-% 2. 
-% 3. 
+% 1. If either the dealer or the player obtains a series of cards that have
+% a combined value > 21, they have "busted" and lost the round.
+% 2. CARD VALUES:
+    % Cards 2 - 10 have a value equal to their face value.
+    % J, Q, K have a value of 10. An Ace can be worth either 1 or 11 based
+    % off of which would bring the player closer to 21 without going over
+% 3. If both player and dealer have not busted, whoever is closer to 21 w/o
+% going over has won the round
+% 4. Players can either hit (take another card), stand (keep their current
+% cards), double down (double their initial bet), split (double their
+% initial bet and turn their two cards into two "separate hands" that are
+% each able to hit for additional cards)
 
 % Summary of Basic Strategy
 
