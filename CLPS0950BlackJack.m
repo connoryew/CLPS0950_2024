50284bed1928eb2be2d608350bf040bde7452946
Connor Yew <134974816+connoryew@users.noreply.github.com>
2024-03-06 14:30:21 -0500

    Rules Updated

--- CLPS0950BlackJack.m
+++ CLPS0950BlackJack.m
@@ -1,19 +1,32 @@
-% CLPS0950 Group Project: How to Play Safe Blackjack (Basic Strategy Sim.)
+% CLPS0950 Group Project: Blackjack (w/ Basic Strategy Recommendations)
 % Group Members: Nicole Chen, Jilienne Widener, Connor Yew
 
 % Rules of BlackJack
-% 1. If either the dealer or the player obtains a series of cards that have
-% a combined value > 21, they have "busted" and lost the round.
-% 2. CARD VALUES:
-    % Cards 2 - 10 have a value equal to their face value.
-    % J, Q, K have a value of 10. An Ace can be worth either 1 or 11 based
-    % off of which would bring the player closer to 21 without going over
-% 3. If both player and dealer have not busted, whoever is closer to 21 w/o
-% going over has won the round
-% 4. Players can either hit (take another card), stand (keep their current
-% cards), double down (double their initial bet), split (double their
-% initial bet and turn their two cards into two "separate hands" that are
+% 1. CARD VALUES:
+    % Cards 2-10 have a value equal to their face value.
+    % J, Q, K eachhave a value of 10. 
+    % An Ace can be worth either 1 or 11 based off of which would bring
+    % the player closer to 21 without going over
+% 2. Each round begins with both the dealer and the player receiving two
+% cards. The player knows the value of both of their own cards, but only
+% knows the value of one of the dealer's cards.
+% 3. If the player is dealt an Ace AND a Face Card (J,Q,or K), that is a
+% BlackJack, and they automatically win the round as long as the dealer
+% does not ALSO have a BlackJack
+% 4. Players can either SURRENDER (fold their intial hand w/o getting any 
+% additional cards), HIT (take another card), STAND (keep their current
+% cards), DOUBLE DOWN (double their initial bet), or SPLIT (double their
+% initial bet and split two equal cards into two "separate hands" that are
 % each able to hit for additional cards)
+% 5. Once the player has decided to STAND on all available hands, the 
+% dealer reveals their hidden card
+% 6. If either the dealer or the player obtains a series of cards that have
+% a combined value > 21, they have "busted" and lost the round.
+% 7. If both player and dealer have NOT busted, whoever is closer to 21 w/o
+% going over has won the round
+% 8. If the player and dealer have NOT busted AND they have cards that sum
+% up to the same value, the result is a PUSH, where neither party wins the
+% round.
 
 % Summary of Basic Strategy
 
--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 **Project Proposal**: For our CLPS0950 Group Project, we will be creating a program that presents the user with a simulated game of BlackJack and allows them to input actions (ie. hold, stay, double-down, surrender, and split) that they would like to take in order to beat the dealer and win the round. Additionally, we will be coding another program that provides recommendations based on the principles of "Basic Strategy" for each hand. These recommendations help to limit the casino's edge over a player to < 1% when properly implemented, and they can be found in a table format at the following link: https://www.blackjackapprenticeship.com/blackjack-strategy-charts/
+
+**Rules of BlackJack**
+
+1. CARD VALUES:
+    • Cards 2-10 have a value equal to their face value.
+    • J, Q, K eachhave a value of 10. 
+    • An Ace can be worth either 1 or 11 based off of which would bring the player closer to 21 without going over
+2. Each round begins with both the dealer and the player receiving two cards. The player knows the value of both of their own cards, but only knows the value of one of the dealer's cards.
+3. If the player is dealt an Ace AND a Face Card (J,Q,or K), they have a BlackJack, and they automatically win the round as long as the dealer does not ALSO have a BlackJack
+4. Players can either SURRENDER (fold their intial hand w/o getting any additional cards), HIT (take another card), STAND (keep their current cards), DOUBLE DOWN (double their initial bet), or SPLIT (double their initial bet and split two equal cards into two "separate hands" that are each able to hit for additional cards)
+5. Once the player has decided to STAND on all available hands, the  dealer reveals their hidden card
+6. If either the dealer or the player obtains a series of cards that have a combined value > 21, they have "busted" and lost the round.
+7. If both player and dealer have NOT busted, whoever is closer to 21 w/o going over has won the round
+8. If the player and dealer have NOT busted AND they have cards that sum up to the same value, the result is a PUSH, where neither party wins the round.
+
+**Summary of Basic Strategy**
+• 
