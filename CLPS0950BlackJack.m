c41042200e8e20c615af8ed3eac7a0d3b475c5cd
Connor Yew <134974816+connoryew@users.noreply.github.com>
2024-03-13 10:44:19 -0400

    Changed pseudocode for setup

--- CLPS0950BlackJack.m
+++ CLPS0950BlackJack.m
@@ -1,21 +1,17 @@
 % CLPS0950 Group Project: Blackjack (w/ Basic Strategy Recommendations)
 % Group Members: Nicole Chen, Jilienne Widener, Connor Yew
 
-%blackjack
-%start with shuffle deck
-shuffle_card = randperm(52);
-% Initialize player and dealer hands
-player_raw_hand = shuffle_card(1:2:3); % dealer gives first and third card to player
-dealer_raw_hand = shuffle_card(2:2:4); % dealer takes second and fourth for themselves
-%card values
-card_value = zeros(1,52); %initialize our card values with 0
+% SETTING UP THE GAME
+shuffle_card = randperm(52); % "Shuffles" the deck by creating a 1x52 vector of random permutations of integers 1-52
+player_raw_hand = shuffle_card(1:2:3); % Deals the 1st and 3rd values from the vector shuffle_card to the player
+dealer_raw_hand = shuffle_card(2:2:4); % Deals the 2nd and 4th values from the vector shuffle card to the dealer
+card_value = zeros(1,52); % Initializes a 1x52 vector of zeroes for the values of the cards
 for card_number = 1:52
-    posit_value = mod(card_number, 13); %posit or position value is the deck position, that we need to map to the game value
+    posit_value = mod(card_number, 13); % Uses the modulo operation to find the position value in the deck, which we'll map to the game value
     if posit_value == 1 %because ace is 1, 1 mod 13 returns 1 --> its position in the modulo operation
         tmp_card_value = 11;
     elseif posit_value > 10 || posit_value == 0 %for jacks, queens, kings have posit value of 0 because matlab matrix starts at 1, meaning ace at 1, meaning king at 13, and 13 mod 13 = 0
         tmp_card_value = 10;
-   
     else
         tmp_card_value = posit_value; %for 2-10, because their rank value within 1:52 corresponds to their actual value
     end
@@ -26,10 +22,12 @@ player_total = sum(card_value(player_raw_hand)); %totalling the two cards player
 dealer_hand = card_value(dealer_raw_hand); %turning raw hand from deck into cards with meaning
 dealer_first_card = card_value(dealer_hand(1)); %show only the first card of the dealer
 
-% TESTING w/ Set Values
+% TESTING/DEBUGGING w/ Set Values
 dealer_first_card = 6
 player_total = 8
 
+
+% HARD TOTALS BASIC STRATEGY
 if dealer_first_card >= 2 && dealer_first_card <=6
     if player_total > 17 
         disp('stand')  
@@ -68,6 +66,13 @@ elseif player_total == 9
 elseif player_total == 8 
     disp('hit');
 end
+
+
+
+%current place: upper half of strategy is coded (12-17 player total, dealer
+%upcard 2-11)
+%what's left in near future: code next round of play 
+
 
 %hit condition psuedocode (writing out the logic first)
 
